29dd5e2f9679051c94f74904103ebaa333ed59d0
_amonsec <amonsec@protonmail.com>
2017-02-26 16:17:44 +0100

    Improvement

--- dnssurgery/dns-zone-transfers.sh
+++ dnssurgery/dns-zone-transfers.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  echo -e "\n[+] DNS zone transfert script"
+  echo -e "\n[+] DNS zone transfer script"
   echo -e "[+] Usage : $0 <domain name>\n"
   exit 0
 fi
 
-echo -e "\n[+] Start zone transfert ...\n"
+echo -e "\n[+] Start zone transfer ...\n"
 for server in $(host -t NS $1 |cut -d" " -f4); do
   host -l $1 $server |grep "has address" |cut -d" " -f1,4
 done
 
-echo -e "\n[+] End zone transfert.\n"
+echo -e "\n[+] End zone transfer.\n"
